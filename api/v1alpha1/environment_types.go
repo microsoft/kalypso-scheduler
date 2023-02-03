1e19e013caf5ea2c0e6e36cc9d699ad7c15aaf1b
Eugene Fedorenko <eugene.fedor@gmail.com>
2023-02-02 18:08:22 -0800

    environment controller

---
+++ api/v1alpha1/environment_types.go
@@ -0,0 +1,59 @@
+/*
+Copyright 2023 microsoft.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// EnvironmentSpec defines the desired state of Environment
+type EnvironmentSpec struct {
+	ControlPlane ManifestsSpec `json:"controlPlane"`
+}
+
+// EnvironmentStatus defines the observed state of Environment
+type EnvironmentStatus struct {
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Environment is the Schema for the environments API
+type Environment struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   EnvironmentSpec   `json:"spec,omitempty"`
+	Status EnvironmentStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// EnvironmentList contains a list of Environment
+type EnvironmentList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Environment `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Environment{}, &EnvironmentList{})
+}
